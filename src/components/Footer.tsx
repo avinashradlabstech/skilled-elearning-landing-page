677c1da1a4e14680fd67d3dbfdd7cf583ef67a68
avinashdahariya <avinash@radlabs.tech>
2025-01-07 08:41:47 +0530

    Update image location

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
     <>
       <div className="flex items-center justify-between self-stretch px-250 py-200 xl:px-1000 xl:py-300 md:py-300 md:px-550 lg:px-1000 bg-blue-900">
         <div className="flex justify-between">
-          <img className="" src="../src/assets/logo-light.svg" alt="Logo"></img>
+          <img className="" src="./logo-light.svg" alt="Logo"></img>
         </div>
         <div className="flex justify-between">
           <div className="gap-100">
--- src/data/info.ts
+++ src/data/info.ts
@@ -28,32 +28,32 @@ const info: Info = {
         'heading': 'Check out our most popular courses!',
         'blockDetails': [
             {
-                'icon': './src/assets/icon-animation.svg',
+                'icon': '/icon-animation.svg',
                 'title': 'Animation',
                 'description': 'Learn the latest animation techniques to create stunning motion design and captivate your audience.',
                 'buttonTxt': 'Get Started'
             },
 
             {
-                'icon': './src/assets/icon-design.svg',
+                'icon': '/icon-design.svg',
                 'title': 'Design',
                 'description': 'Create beautiful, usable interfaces to help shape the future of how the web looks.',
                 'buttonTxt': 'Get Started'
             },
             {
-                'icon': './src/assets/icon-photography.svg',
+                'icon': '/icon-photography.svg',
                 'title': 'Photography',
                 'description': 'Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.',
                 'buttonTxt': 'Get Started'
             },
             {
-                'icon': './src/assets/icon-crypto.svg',
+                'icon': '/icon-crypto.svg',
                 'title': 'Crypto',
                 'description': 'All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course.',
                 'buttonTxt': 'Get Started'
             },
             {
-                'icon': './src/assets/icon-business.svg',
+                'icon': '/icon-business.svg',
                 'title': 'Business',
                 'description': 'A step-by-step playbook to help you start, scale, and sustain your business without outside investment.',
                 'buttonTxt': 'Get Started'
