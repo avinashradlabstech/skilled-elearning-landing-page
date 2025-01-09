5de2ea55ab1354cfdfebc18cc361fb2daf0e8092
avinashdahariya <avinash@radlabs.tech>
2025-01-09 10:29:34 +0530

    Fixed Hero text alignment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,8 +7,8 @@ const Hero: React.FC = () => {
   return (
     <>
       <div className="her-section flex flex-col items-center justify-center gap-100 self-stretch px-250 py-800 md:py-800 md:px-550 lg:px-1000 lg:py-800 relative overflow-x-clip">
-        <div className="flex flex-col md:flex-row items-center justify-center gap-600 md:gap-300 lg:gap-400 max-w-[1120px] mx-auto ">
-          <div className="hero-text-section flex flex-col items-start gap-500 md:w-1/2">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-600 md:gap-300 md:min-h-[400px] lg:gap-400 lg:max-w-[1120px] lg:min-h-[560px]">
+          <div className="hero-text-section flex flex-col items-start gap-500 md:w-1/2 md:justify-center">
             <div className="flex flex-col items-start gap-300 lg:gap-500">
               <div className="font-sans font-extrabold leading-48 text-blue-900 text-40 lg:text-56 lg:leading-67.2">
                 {info.heroData.heading}
